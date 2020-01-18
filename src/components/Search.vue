fb6061ec096aa3f2d9d3ffee1b8cf12876f91794
PhilippD <dippelp@hochschule-trier.de>
2020-01-18 12:46:06 +0100

    Add Search Component

---
+++ src/components/Search.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="searchbar">
+        <span class="icon-search"></span>
+        <input v-model="searchText" type="search" name="search" > 
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Search"
+}
+</script>
+
+<style lang="scss">
+    input[type=search] {
+        margin-left: 10px;
+        padding: 10px;
+        height: 100%;
+    }
+</style>
